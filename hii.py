7e49d4d94f10da7812aef6db22a8bc02d864291b
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-18 19:40:03 +0530

    variable

--- hii.py
+++ hii.py
@@ -1,2 +1,11 @@
-print('Hello, world!')
-print("Nirbhay is here")
+# print('Hello, world!')
+# print("Nirbhay is here")
+
+name = "Nirbhay";
+age = 21;
+developer = True;
+language = ["Html","css","javascript","Typescript","Nodejs", "Reactjs"];
+print(type(name));
+print(type(age));
+print(type(developer));
+print(type(language))
